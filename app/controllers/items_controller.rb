0b8d288ffde9db4b0890bdcc4f9d07b7bc4c73c8
Keelana Char <keelanachar@gmail.com>
2019-11-26 23:01:53 -0600

    implemented a frontend create/new action. redirect to show action page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,17 @@ class ItemsController < ApplicationController
     @item = Item.find_by(id: params[:id])
     render 'show.html.erb'
   end
+  def new
+    render 'new.html.erb'
+  end
+  def create
+    @item = Item.new(
+      name: params[:item_name],
+      description: params[:description],
+      price: params[:price],
+      supplier_id: params[:supplier_id]
+    )
+    @item.save
+    redirect_to "/items/#{@item.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,7 @@ Rails.application.routes.draw do
 
   end
   get "/items" => "items#index"
-  
+  get "/items/new" => "items#new"
+  post "/items" => "items#create"
   get "/items/:id" => "items#show"
 end
